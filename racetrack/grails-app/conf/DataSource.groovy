fa9e788db8d83e5c388c4386220a887aa3ec34fc
Rommel Rico <rommeltj@gmail.com>
2014-09-09 12:01:37 -0700

    Umm

--- racetrack/grails-app/conf/DataSource.groovy
+++ racetrack/grails-app/conf/DataSource.groovy
@@ -3,7 +3,7 @@ dataSource {
     jmxExport = true
     driverClassName = "com.mysql.jdbc.Driver"
     username = "romri"
-    password = "9zs6scKweb!"
+    password = password
 }
 hibernate {
     cache.use_second_level_cache = true
