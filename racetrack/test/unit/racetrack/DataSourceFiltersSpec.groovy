9264d1df0fd8efcdbc494ce1c24198e9a7e65509
Rommel Rico <rommeltj@gmail.com>
2014-09-10 22:02:42 -0700

    Temp

---
+++ racetrack/test/unit/racetrack/DataSourceFiltersSpec.groovy
@@ -0,0 +1,17 @@
+package racetrack
+
+import grails.test.mixin.Mock
+import spock.lang.Specification
+
+@Mock(DataSourceFilters)
+class DataSourceFiltersSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
